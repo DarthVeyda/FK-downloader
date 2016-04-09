81f379bd1128e9766dc00f7381cf27e549dea46a
DarthVeyda <veydaster@gmail.com>
2016-04-09 20:20:05 +1000

    Initial commit - project created

---
+++ FK-Downloader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace FK_Downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl("file:///F:/!tmp/Diary/Diary.ru%20-%20Mobile%20v2.htm#mainPage");
+                driver.FindElement(By.Id("username")).SendKeys("ФБ: Гость");
+                driver.FindElement(By.Id("password")).SendKeys("zxcvbn");
+                driver.FindElement(By.Id("loginbutton")).Click();
+                driver.Quit();
+            }
+        }
+    }
+}
