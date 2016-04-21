816c3e1079f4379f76ccb66a7a54e19964d2bcd7
DarthVeyda <veydaster@gmail.com>
2016-04-21 17:16:57 +1000

    Saving the whole tree as raw html files to parse later

--- FK-Downloader/Program.cs
+++ FK-Downloader/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Interactions;
 
 namespace FK_Downloader
 {
     class Program
-    {
+    {        
         //TODO - everything below goes to configuration or input params
-        private static string saveFolder = @"f:\data\FK_curr";
+        private const string FandomText = "fandom";
+        private const string LevelText = "левел";
+        private const string CommentBoxId = "message";
+        private const string saveFolder = @"f:\data\FK_curr";
         private static string FKMasterURL;
-        private static readonly string tags = "?tags";
-        private static readonly string openCut = "?oam";
-        private static readonly string postLink = "URL";
-        private static readonly string nextPage = "from=";
-        private static readonly int postsPerPage = 20;
+        private const string tags = "?tags";
+        private const string openCut = "?oam";
+        private const string postLink = "URL";
+        private const string nextPage = "from=";
+        private const int postsPerPage = 20;
 
 
         private static readonly string[] questList = //TODO in proper application this will be taken from the input. 
@@ -67,45 +69,35 @@ namespace FK_Downloader
             var firefoxProfile = new FirefoxProfile { EnableNativeEvents = false };
             using (IWebDriver driver = new FirefoxDriver(firefoxProfile))
             {
-                FKMasterURL = "http://fk-2o15.diary.ru";
+                FKMasterURL = args[0];
 
                 driver.Navigate().GoToUrl(new Uri(FKMasterURL + tags));
 
-                driver.FindElement(By.Id("usrlog2")).SendKeys("littleqwerty");
-                driver.FindElement(By.Id("usrpass2")).SendKeys("дшеедуйцукен");
+                driver.FindElement(By.Id("usrlog2")).SendKeys(args[1]);
+                driver.FindElement(By.Id("usrpass2")).SendKeys(args[2]);
                 driver.FindElement(By.ClassName("submit")).Click();
 
                 var wait = new DefaultWait<IWebDriver>(driver) { Timeout = new TimeSpan(0, 0, 0, 10) };
                 wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
-                wait.Until(ExpectedConditions.ElementIsVisible(By.PartialLinkText("fandom")));
+                wait.Until(ExpectedConditions.ElementIsVisible(By.PartialLinkText(FandomText)));
 
                 //Format for multiple tag selection
                 //http://fk-2o15.diary.ru/?tag[]=26629&tag[]=5300355&from=60
 
                 var quests = CreateTagList(driver, questList);
-                var levels = CreateTagList(driver, "левел").Skip(1); //don't need Level 1 for now
-                var fandoms = CreateTagList(driver, "fandom");
+                var levels = CreateTagList(driver, LevelText).Skip(1); //don't need Level 1 for now
+                var fandomsFinal = CreateTagList(driver, FandomText).Select(fandom => new FandomStructure(fandom.Key, fandom.Value)).ToList();
 
-                var levelsFinal = new List<LevelStructure>();
-                //Check each tag combination to see if it returns any posts, and throw it away if it doesn't
-                foreach (var level in levels)
-                {
-                    var tmpQuests = (from quest in quests
-                                     let postsExist = PostsExist(driver, FKMasterURL + "?" + level.Value + "&" + quest.Value)
-                                     where postsExist
-                                     select new Quest(quest.Key, quest.Value)).ToList();
-                    var tmpLevel = new LevelStructure(level.Key, level.Value);
-                    tmpLevel.AddQuestList(tmpQuests);
-                    levelsFinal.Add(tmpLevel);
-                }
+                var levelsFinal = CreateLevelQuestTree(levels, quests, driver);
 
-                var fandomsFinal = new List<FandomStructure>();
-                foreach (var tmpFandom in fandoms.Select(fandom => new FandomStructure(fandom.Key, fandom.Value)))
+                foreach (var tmpFandom in fandomsFinal)
                 {
                     tmpFandom.AddLevelList(levelsFinal);
-                    fandomsFinal.Add(tmpFandom);
                 }
 
+                //FOR DEBUGGING//////
+                fandomsFinal = new List<FandomStructure>() { fandomsFinal.First() };
+                /////////////////////
                 foreach (var fandom in fandomsFinal)
                 {
                     if (!Directory.Exists(fandom.Name)) Directory.CreateDirectory(fandom.Name);
@@ -130,6 +122,24 @@ namespace FK_Downloader
                                 tmpPosts = GetPosts(driver, url + "&" + nextPage + skipPages);
                             } 
                             quest.AddURLList(posts);
+                            foreach (var postUrL in quest.PostURLs)
+                            {
+                                driver.Navigate().GoToUrl(postUrL + openCut);
+                                //wait until the comments form is there - it is the last one to be loaded
+                                wait = new DefaultWait<IWebDriver>(driver) { Timeout = new TimeSpan(0, 0, 0, 60) };
+                                wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+                                wait.Until(ExpectedConditions.ElementIsVisible(By.Id(CommentBoxId)));
+
+                                using (
+                                    StreamWriter rawHTML =
+                                        new StreamWriter(Path.Combine(Directory.GetCurrentDirectory(),
+                                            new Uri(postUrL).MakeRelativeUri(new Uri(FKMasterURL))
+                                                .GetLeftPart(UriPartial.Path))))
+                                {
+                                    rawHTML.Write(driver.PageSource);
+                                }
+
+                            }
                         }                       
                         Directory.SetCurrentDirectory("..");
                     }
@@ -138,6 +148,22 @@ namespace FK_Downloader
 
                 driver.Quit();
             }
+        }
+
+        private static List<LevelStructure> CreateLevelQuestTree(IEnumerable<KeyValuePair<string, string>> levels, Dictionary<string, string> quests, IWebDriver driver)
+        {
+            var levelsFinal = new List<LevelStructure>();
+            //Check each tag combination to see if it returns any posts, and throw it away if it doesn't
+            foreach (var level in levels)
+            {
+                var tmpQuests = (from quest in quests
+                    where PostsExist(driver, FKMasterURL + "?" + level.Value + "&" + quest.Value)
+                    select new Quest(quest.Key, quest.Value)).ToList();
+                var tmpLevel = new LevelStructure(level.Key, level.Value);
+                tmpLevel.AddQuestList(tmpQuests);
+                levelsFinal.Add(tmpLevel);
+            }
+            return levelsFinal;
         }
 
         private static List<string> GetPosts(IWebDriver driver, string url)
@@ -149,14 +175,12 @@ namespace FK_Downloader
 
         private static bool PostsExist(IWebDriver driver, string url)
         {
-            DefaultWait<IWebDriver> wait;
             driver.Navigate().GoToUrl(new Uri(url));
             bool postsExist = false;
             try
             {
-                wait = new DefaultWait<IWebDriver>(driver) { Timeout = new TimeSpan(0, 0, 0, 30) };
+                var wait = new DefaultWait<IWebDriver>(driver) { Timeout = new TimeSpan(0, 0, 0, 30) };
                 wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
-                wait.IgnoreExceptionTypes(typeof(WebDriverException));
                 wait.Until(ExpectedConditions.ElementIsVisible(By.PartialLinkText(postLink)));
                 postsExist = true;
             }
@@ -168,13 +192,9 @@ namespace FK_Downloader
             catch (WebDriverException)
             {
                 // in our case the page most likely takes too long to load because of 
-                // embedded media and/or large images. We don't need any of those at this stage,
-                // so it's safe to stop loading and search again
-                Actions actions = new Actions(driver);
-                actions.SendKeys(Keys.Escape);
-                wait = new DefaultWait<IWebDriver>(driver) { Timeout = new TimeSpan(0, 0, 0, 30) };
-                wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
-                wait.Until(ExpectedConditions.ElementIsVisible(By.PartialLinkText(postLink)));
+                // embedded media and/or large images - which means there are posts on the page.
+                // None of the workarounds meant to stop loading the page manually seem to work anyway.
+
                 postsExist = true;
             }
             return postsExist;
